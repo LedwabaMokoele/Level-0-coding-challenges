ae8d771e00d2a942766be6f43fb3e2ce6dda2b4d
LedwabaMokoele <william.mokoele@gmail.com>
2021-09-23 02:30:02 +0200

    changed max from default value 0 to default value of the first element in the list passed

--- Task_06.py
+++ Task_06.py
@@ -1,5 +1,5 @@
 def maximum(num_1,*nums):
-    max = 0
+    max = num_1
     for num_i in nums:
         if(num_i > max):
             max = num_i
@@ -8,5 +8,5 @@ def maximum(num_1,*nums):
     else:
         return max
 
-max = maximum(4,7,32,27,22,96,1,3,32)
+max = maximum(0,-7,-32,-27,-22,96,1,3,32)
 print(max)
