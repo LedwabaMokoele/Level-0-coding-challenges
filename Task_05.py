c024924ff0714aae010b04e652b7e90ca2a9b07f
LedwabaMokoele <william.mokoele@gmail.com>
2021-09-23 02:26:41 +0200

    removed built-in function

--- Task_05.py
+++ Task_05.py
@@ -3,7 +3,7 @@ def area_of_triangle(x,y,z):
     s_length = (x+y+z)/2
     area_t = (s_length-x)*(s_length-y)*(s_length-z)
     area_t = area_t*s_length
-    area_t = math.sqrt(area_t)
+    area_t = area_t**(0.5)
     return area_t
 
 
