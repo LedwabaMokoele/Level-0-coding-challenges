5cd2f30b3f134dbfb81f5eea78a0471d914f9cd4
LedwabaMokoele <william.mokoele@gmail.com>
2021-09-23 02:48:01 +0200

    Fixed the code so that it prints out distinct letters that are common to both words

--- Task_10.py
+++ Task_10.py
@@ -1,13 +1,16 @@
 import string
 def print_Common(word_i, word_ii):
     common = "Common letters: "
+    common_letters = []
     temp_string_i = word_i.lower()  #temporary strings 
     temp_string_ii = word_ii.lower() #to store the variables passed
     for i in temp_string_ii:  
-        if(temp_string_i.count(i) > 0):
+        if(temp_string_i.count(i) > 0 and common_letters.count(i) == 0):
             if(i != " "):
-                common+=i+", "
-    common= common[:-2] #Remove the extra comma and space at the end
+                common_letters.append(i)
+    for i in common_letters:
+        common += str(i)+", "
+    common = common[:-2] #Remove the extra comma and space at the end
     return common
 
-print(print_Common("house","computers")) 
+print(print_Common("Fearers","Terry")) 
